250c8493045bc45502bab27608de299564037a41
paigeingram <paigehingram@gmail.com>
2021-10-14 14:49:42 -0700

    Created tests

--- src/main/test/PlaylistTest.java
+++ src/main/test/PlaylistTest.java
@@ -1,6 +1,41 @@
 package test;
 
+import model.Playlist;
+import model.Song;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PlaylistTest {
+    private Playlist playlist;
+    private Song testsong;
 
+    @BeforeEach
+    void setup() {
+        playlist = new Playlist("Test playlist");
+        testsong = new Song("Test song", "Test artist");
+    }
 
+    @Test
+    void testGetListOfSongsEmpty() {
+        assertEquals(null, playlist.getListOfSongs());
+    }
+
+    @Test
+    void testGetListOfSongs() {
+        playlist.addSong(testsong);
+        assertEquals(testsong, playlist.getListOfSongs());
+    }
+
+    @Test
+    void testGetPlaylistName() {
+        assertEquals("Test playlist", playlist.getPlaylistName());
+    }
+
+    @Test
+    void testAddSong() {
+        playlist.addSong(testsong);
+        assertEquals(testsong, playlist.getListOfSongs());
+    }
 }
--- src/main/test/SongTest.java
+++ src/main/test/SongTest.java
@@ -2,21 +2,25 @@ package test;
 
 import model.Song;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SongTest {
-    Song song;
+    private Song song;
 
     @BeforeEach
     public void setup() {
         song = new Song("Song name", "Song artist");
     }
 
-
-    public void testGetName() {
-
+    @Test
+    void testGetName() {
+        assertEquals("Song name", song.getName());
     }
 
-
-
-
+    @Test
+    void testGetArtist() {
+        assertEquals("Song artist", song.getArtist());
+    }
 }
