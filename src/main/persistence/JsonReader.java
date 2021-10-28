1de86236aab813c927648d7de39ef4c0f32aefc5
paigeingram <paigehingram@gmail.com>
2021-10-28 15:41:47 -0700

    removed buggy import statement

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.stream.Stream;
-
-import com.sun.tools.javac.util.List;
 import model.MusicPlayer;
 import model.Playlist;
 import model.Song;
--- src/main/persistence/JsonTest.java
+++ src/main/persistence/JsonTest.java
@@ -1,6 +1,5 @@
 package persistence;
 
-import com.sun.tools.javac.util.List;
 import model.Playlist;
 import model.Song;
 
@@ -10,13 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Citation: JsonSerializationDemo - JsonTest.java
 public class JsonTest {
-    protected void checkSong(String songName, String artistName, Song song) {
-        assertEquals(songName, song.getName());
-        assertEquals(artistName, song.getArtist());
-    }
 
     protected void checkPlaylist(String playlistName, ArrayList<Song> listOfSongs, Playlist playlist) {
         assertEquals(playlistName, playlist.getPlaylistName());
         assertEquals(listOfSongs.size(), playlist.getListOfSongs().size());
     }
+
+//    protected void checkSong(String songName, String artistName, Song song) {
+//        assertEquals(songName, song.getName());
+//        assertEquals(artistName, song.getArtist());
+//    }
 }
