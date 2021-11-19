ef10fb3d4aa3d46660ce18a3c0d3a4bd958bb9fe
paigeingram <paigehingram@gmail.com>
2021-11-19 11:56:46 -0800

    cleaned up code comments and formatting

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -22,12 +22,8 @@ import java.util.List;
 // https://github.students.cs.ubc.ca/CPSC210/TellerApp.git
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
 
+// user interface for the music player application
 public class GUI extends JFrame {
-
-// Represents the main window in which the to-do list exists
-
-    public static final int WIDTH = 1000;
-    public static final int HEIGHT = 700;
 
     private static final String JSON_STORE = "./data/MusicPlayer.json";
     private JList list;
@@ -35,12 +31,6 @@ public class GUI extends JFrame {
     private MusicPlayer mp;
     private JsonWriter jsonWriter = new JsonWriter(JSON_STORE);
     private JsonReader jsonReader = new JsonReader(JSON_STORE);
-
-
-    private JFrame frame;
-    private JPanel topPanel;
-    private JPanel btmPanel;
-    private JScrollPane scrollPane;
 
     private JButton newPlystBtn;
     private JButton addSongBtn;
@@ -52,7 +42,7 @@ public class GUI extends JFrame {
     // Constructs main window
     // MODIFIES: this
     // EFFECTS: sets up window in which music player will be used
-    public GUI() { // does GUI take a table param
+    public GUI() {
         super("Music player");
         this.mp = new MusicPlayer("My Music Player");
         initializeGraphics();
@@ -64,64 +54,32 @@ public class GUI extends JFrame {
         initializeScrollPane();
         initializeGraphics();
         initializeButtons();
-
-//        setSize(300, 200);
-//        setBackground(Color.blue);
-
-//        frame = new JFrame();
-//        topPanel = new JPanel();
-//        btmPanel = new JPanel();
-//
-//        topPanel.setLayout(new BorderLayout());
-//        getContentPane().add(topPanel, BorderLayout.CENTER);
-//        getContentPane().add(btmPanel, BorderLayout.SOUTH);
-//        scrollPane = new JScrollPane();
-//        topPanel.add(scrollPane, BorderLayout.CENTER);
-//
-//        JPanel panel = new JPanel();
-//        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
-//        panel.setLayout(new GridLayout(1, 1));
-//
-//        frame.add(topPanel, BorderLayout.CENTER);
-//        frame.add(btmPanel, BorderLayout.CENTER);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setTitle("Music Player");
-//        frame.pack();
-//        frame.setVisible(true);
-//
-//        initializeButtons();
     }
 
+    // Code modelled on DrawingPlayer application:
+    // https://github.students.cs.ubc.ca/CPSC210/SimpleDrawingPlayer-Complete
     // MODIFIES: this
     // EFFECTS: creates scroll pane and inserts list component
     private void initializeScrollPane() {
-
         listModel = new DefaultListModel();
         list = new JList(listModel);
         JScrollPane listScrollPane = new JScrollPane(list);
         add(listScrollPane);
         setVisible(true);
-
     }
-    // MODIFIES: this
-    // EFFECTS:  draws the JFrame window where this DrawingEditor will operate, and populates the tools to be used
-    //           to manipulate this drawing
 
+
+    // Code modelled on DrawingPlayer application:
+    // https://github.students.cs.ubc.ca/CPSC210/SimpleDrawingPlayer-Complete
+    // MODIFIES: this
+    // EFFECTS:  initializes settings for JFrame
     private void initializeGraphics() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         centreOnScreen();
         setVisible(true);
-
-//        setLayout(new BorderLayout());
-//        setMinimumSize(new Dimension(WIDTH, HEIGHT));
-//        initializeButtons();
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setLocationRelativeTo(null);
-//        setVisible(true);
     }
 
-    // Code modeled on space invaders application: https://github.students.cs.ubc.ca/CPSC210/B02-SpaceInvadersBase.git
-
+    // Code modeled on SpaceInvaders application: https://github.students.cs.ubc.ca/CPSC210/B02-SpaceInvadersBase.git
     // MODIFIES: this
     // EFFECTS: location of frame is set so frame is centered on desktop
     private void centreOnScreen() {
@@ -129,51 +87,45 @@ public class GUI extends JFrame {
         setLocation((scrn.width - getWidth()) / 2, (scrn.height - getHeight()) / 2);
     }
 
+    // Code modeled on stack exchange:
+    // https://stackoverflow.com/questions/284899/how-do-you-add-an-actionlistener-onto-a-jbutton-in-java
     // MODIFIES: this
     // EFFECTS: initializes buttons
     public void initializeButtons() {
         newPlystBtn = new JButton("New Playlist");
-        //      newPlystBtn.setBounds(0, 300, 130, 30);
         newPlystBtn.addActionListener(this::newPlaylist);
         add(newPlystBtn);
 
-
         addSongBtn = new JButton("Add Song");
-        //  addSongBtn.setBounds(125, 300, 100, 30);
         addSongBtn.addActionListener(this::addSong);
         add(addSongBtn);
 
         removeSongBtn = new JButton("Remove Song");
-        // removeSongBtn.setBounds(225, 300, 130, 30);
         removeSongBtn.addActionListener(this::removeSong);
         add(removeSongBtn);
 
         viewPlystBtn = new JButton("View Playlist");
-//        viewPlystBtn.setBounds(225, 300, 130, 30);
         viewPlystBtn.addActionListener(this::viewPlaylist);
         add(viewPlystBtn);
 
         saveMscPlyrBtn = new JButton("Save Music Player");
-//        saveMscPlyrBtn.setBounds(225, 300, 130, 30);
         saveMscPlyrBtn.addActionListener(this::saveMusicPlayer);
         add(saveMscPlyrBtn);
 
         loadMscPlyrBtn = new JButton("Load Music Player");
-        //      loadMscPlyrBtn.setBounds(225, 300, 130, 30);
         loadMscPlyrBtn.addActionListener(this::loadMusicPlayer);
         add(loadMscPlyrBtn);
 
         pack();
         setVisible(true);
-
         setSize(1250, 1000);
         setLayout(null);
         setVisible(true);
     }
 
     // MODIFIES: this
-    // EFFECTS: pops up JOptionPanes for user to input details about a task
-    //          adds task to the to-do list
+    // EFFECTS: pops up JOptionPanes for user to input details about a playlist
+    //          adds playlist to the music player
     public void newPlaylist(ActionEvent e) {
         JFrame addNamePanel = new JFrame();
         JFrame successfullyAddedPanel = new JFrame();
@@ -190,16 +142,14 @@ public class GUI extends JFrame {
         }
     }
 
-    // EFFECTS: displays specified playlist in new JFrame
+    // EFFECTS: displays specified playlist in JOptionPanes
     public void viewPlaylist(ActionEvent e) {
         JFrame viewPlaylistPosition = new JFrame();
         JFrame showPlaylistPanel = new JFrame();
-        JFrame playlistImage = new JFrame();
 
         String playlistPosAsString = JOptionPane.showInputDialog(viewPlaylistPosition,
                 "Which playlist would you like to view? "
                         + "Please enter the playlist's position in the list (0 indexing).");
-//        Playlist playlistToView = getPlaylist(playlistPosAsString);
         if (playlistPosAsString != null) {
             try {
                 if (getPlaylist(playlistPosAsString).getListOfSongs().size() == 0) {
@@ -212,15 +162,6 @@ public class GUI extends JFrame {
                             + playlistToView.getPlaylistName() + "\n"
                             + showPlaylist(playlistToView));
                     showImage();
-//                    ImageIcon icon = new ImageIcon("/data/MusicPlayerImage.png");
-//                    JOptionPane.showMessageDialog(playlistImage, icon);
-//                    List<Song> listOfSongs = playlistToView.getListOfSongs();
-//                    for (int s = 0; s < numSongs; s++) {
-//                        Song song = listOfSongs.get(s);
-//                        String songName = song.getName();
-//                        String artistName = song.getArtist();
-//                        JOptionPane.showMessageDialog(showPlaylistPanel, "'" + songName + "' by " + artistName);
-//                    }
                 }
             } catch (InvalidPositionException exception) {
                 JOptionPane.showMessageDialog(showPlaylistPanel,
@@ -229,6 +170,7 @@ public class GUI extends JFrame {
         }
     }
 
+    // EFFECTS: iterates through list of songs in given playlist and shows each song name and artist
     private String showPlaylist(Playlist playlist) {
         int numSongs = playlist.getListOfSongs().size();
         List<Song> listOfSongs = playlist.getListOfSongs();
@@ -250,35 +192,22 @@ public class GUI extends JFrame {
         return playlistToGet;
     }
 
-    // The following code is modeled on ListDemo.java example from:
-    // https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/uiswing
-    // /examples/components/ListDemoProject/src/components/ListDemo.java
 
-
-//    private void initializeInteraction() {
-//        MusicPlayerListener mpl = new MouseMotionListener();
-//        addMouseListener(mpl);
-//        addMouseMotionListener(mpl);
-//    }
-
-
-    // EFFECTS: saves list to file
+    // EFFECTS: saves music player to file
     public void saveMusicPlayer(ActionEvent e) {
         JFrame writeToFilePanel = new JFrame();
         try {
             jsonWriter.open();
             jsonWriter.write(mp);
             jsonWriter.close();
-
             JOptionPane.showMessageDialog(writeToFilePanel, "Saved your playlists to " + JSON_STORE);
-
         } catch (FileNotFoundException exception) {
             JOptionPane.showMessageDialog(writeToFilePanel, "Unable to write to file: " + JSON_STORE);
         }
     }
 
     // MODIFIES: this
-    // EFFECTS: loads to-do list from file
+    // EFFECTS: loads music player from file
     public void loadMusicPlayer(ActionEvent e) {
         JFrame readFromFilePanel = new JFrame();
         try {
@@ -288,30 +217,27 @@ public class GUI extends JFrame {
         } catch (IOException exception) {
             JOptionPane.showMessageDialog(readFromFilePanel, "Unable to read from file: " + JSON_STORE);
         }
-
         for (int p = 0; p < mp.getListOfPlaylists().size(); p++) {
             listModel.addElement(mp.getListOfPlaylists().get(p).getPlaylistName());
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: pops up JOptionPanes for user to input details about a song
+    // adds song to a playlist in the music player
     public void addSong(ActionEvent e) {
         Playlist selectedPlaylist = selectPlaylist();
 
-        //      JFrame viewSelectedPlaylist = new JFrame();
         JFrame addNamePanel = new JFrame();
         JFrame addArtistPanel = new JFrame();
         JFrame successfullyAddedPanel = new JFrame();
         JFrame failureToAddPanel = new JFrame();
-
-        //viewPlaylistToAddSong(selectedPlaylist);
 
         String songName = JOptionPane.showInputDialog(addNamePanel,
                 "Please enter the name of the song to add to " + selectedPlaylist.getPlaylistName() + ".");
         if (songName != null) {
             String artistName = JOptionPane.showInputDialog(addArtistPanel,
                     "Please enter the artist's name.");
-
-
             Song addedSong = new Song(songName, artistName);
             selectedPlaylist.addSong(addedSong);
             JOptionPane.showMessageDialog(successfullyAddedPanel,
@@ -323,6 +249,7 @@ public class GUI extends JFrame {
         }
     }
 
+    // EFFECTS: pops up JOptionPane for user to input details which playlist they will add their song to
     public Playlist selectPlaylist() {
         JFrame selectPlaylistPanel = new JFrame();
         String playlistPosAsString = JOptionPane.showInputDialog(selectPlaylistPanel,
@@ -333,37 +260,25 @@ public class GUI extends JFrame {
         return mp.getListOfPlaylists().get(playlistChoice);
     }
 
-    public void viewPlaylistToAddSong(Playlist playlist) {
-        JFrame playlistAddSongPanel = new JFrame();
-
-    }
-
+    // EFFECTS: pops up JOptionPane for user to input details which playlist they would like to remove a song from
     public void removeSong(ActionEvent e) {
-
         JFrame insertPlaylistPosition = new JFrame();
-        JFrame showPlaylist = new JFrame();
-
         JFrame removeSongPanel = new JFrame();
-        JFrame successfullyAddedPanel = new JFrame();
-
-        String removeSongIndex;
-
-
         String playlistPosAsString = JOptionPane.showInputDialog(removeSongPanel,
                 "Which playlist would you like to remove a song from? "
                         + "Please enter the playlist's position in the list (0 indexing).");
-//        Playlist playlistToView = getPlaylist(playlistPosAsString);
         if (playlistPosAsString != null) {
             try {
                 viewPlaylistToRemoveSong((getPlaylist(playlistPosAsString)));
             } catch (InvalidPositionException exception) {
                 JOptionPane.showMessageDialog(insertPlaylistPosition,
                         "That is not a valid position.");
-
             }
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: pops up JOptionPane for user to input details which song they would like to remove
     public void viewPlaylistToRemoveSong(Playlist playlist) {
         JFrame selectSongPanel = new JFrame();
 
@@ -380,11 +295,10 @@ public class GUI extends JFrame {
         } catch (InvalidPositionException exception) {
             JOptionPane.showMessageDialog(selectSongPanel,
                     "That is not a valid position.");
-
         }
     }
 
-    // EFFECTS: returns task of position passed as argument
+    // EFFECTS: returns song of position passed as argument
     //          throws an InvalidPositionException if position doesn't exist
     private Song getSong(Playlist playlist, String songPosAsString) throws InvalidPositionException {
         Integer songPosAsInt = Integer.parseInt(songPosAsString);
@@ -393,8 +307,9 @@ public class GUI extends JFrame {
         return songToRemove;
     }
 
+    // MODIFIES: this
+    // EFFECTS: displays "yay!" image after playlist is viewed
     private void showImage() {
-
         JDialog yayDialog = new JDialog();
         yayDialog.setUndecorated(true);
         JLabel label = new JLabel(new ImageIcon("data/yay.png"));
@@ -403,7 +318,6 @@ public class GUI extends JFrame {
         yayDialog.setVisible(true);
         JDialog playlistDialog = new JDialog();
         playlistDialog.setUndecorated(true);
-
         JOptionPane.showMessageDialog(yayDialog, label,
                 "Yay! What an amazing playlist!",
                 JOptionPane.PLAIN_MESSAGE, null);
