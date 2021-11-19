9e1c5df8b7a0f1a41689509d025776e904f47be9
paigeingram <paigehingram@gmail.com>
2021-11-19 11:58:47 -0800

    cleaned up code comments and formatting

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,16 +1,8 @@
 package ui;
 
-import java.io.FileNotFoundException;
-
 public class Main {
 
     public static void main(String[] args) {
         new GUI();
-
-//        try {
-//            new MusicPlayerApp();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Unable to run application: file not found");
-//        }
     }
 }
